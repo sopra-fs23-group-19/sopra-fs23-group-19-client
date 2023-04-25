c28437346d6f4b912dca174db6ec88a587b130a1
Jingjing Li <jingjing.li@uzh.ch>
2023-04-25 23:54:12 +0200

    change drawing line

--- src/components/views/DrawingBoard.js
+++ src/components/views/DrawingBoard.js
@@ -9,10 +9,10 @@ import { useHistory } from "react-router-dom";
 const DrawingBoard = ({ role, time, hanleDoSubmit }) => {
   const history = useHistory();
   const setConvasRef = useOnDraw(onDraw);
-  // const [lineColor, setLineColor] = useState();
-  // const [lineWidth, setLineWidth] = useState();
-  let lineColor;
-  let lineWidth;
+  const [lineColor, setLineColor] = useState(null);
+  const [lineWidth, setLineWidth] = useState(null);
+  // var lineColor;
+  // var lineWidth;
   // const [cursorStyle, setCursorStyle] = useState(null);
   //   console.log(cursorStyle);
   // let cursorStyle = "url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto"
@@ -49,9 +49,11 @@ const DrawingBoard = ({ role, time, hanleDoSubmit }) => {
         const ctx = canvas.getContext("2d");
         ctx.fillStyle = "#ffffff";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        lineColor='#000000';
-        lineWidth=5;
-        document.querySelector("#board").style.cursor = "pointer"
+        // lineColor='#000000';
+        // lineWidth=5;
+        setLineColor('#000000');
+        setLineWidth(5);
+        document.querySelector("#board").style.cursor = "pointer";
         // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto");
       }
     }
@@ -107,14 +109,15 @@ const DrawingBoard = ({ role, time, hanleDoSubmit }) => {
     canvasObject.beginPath();
     canvasObject.lineWidth = width;
     canvasObject.strokeStyle = color;
+    canvasObject.fillStyle = color;
     canvasObject.moveTo(start.x, start.y);
     canvasObject.lineTo(end.x, end.y);
     canvasObject.stroke();
 
-    canvasObject.fillStyle = color;
-    canvasObject.beginPath();
-    canvasObject.arc(start.x, start.y, width * 0.45, 0, width * Math.PI);
-    canvasObject.fill();
+    // canvasObject.fillStyle = color;
+    // canvasObject.beginPath();
+    // canvasObject.arc(start.x, start.y, width * 0.45, 0, width * Math.PI);
+    // canvasObject.fill();
   }
 
   // function doGetDrawing(){
@@ -161,74 +164,82 @@ const DrawingBoard = ({ role, time, hanleDoSubmit }) => {
   //     ctx.fillRect(0, 0, canvas.width, canvas.height);
   // }
 
-  const redPen = async () => {
-    lineColor = "#FF0000";
+  function  redPen(){
+    // lineColor = "#FF0000";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#FF0000");
+    console.log(lineColor);
+    setLineColor("#FF0000");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto");
   };
 
-  const orangePen = async () => {
-    lineColor = "#FF7B00";
+  function orangePen() {
+    // lineColor = "#FF7B00";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#FF7B00");
+    console.log(lineColor);
+    setLineColor("#FF7B00");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto");
   };
 
-  const yellowPen = async () => {
-    lineColor = "#FFFF00";
+  function yellowPen() {
+    // lineColor = "#FFFF00";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#FFFF00");
+    console.log(lineColor);
+    setLineColor("#FFFF00");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto")
   };
 
-  const greenPen = async () => {
-    lineColor = "#00FF00";
+  function greenPen() {
+    // lineColor = "#00FF00";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#00FF00");
+    console.log(lineColor);
+    setLineColor("#00FF00");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto")
   };
 
-  const bluePen = async () => {
-    lineColor = "#0000FF";
+  function bluePen() {
+    // lineColor = "#0000FF";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#0000FF");
+    console.log(lineColor);
+    setLineColor("#0000FF");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto")
   };
 
-  const purplePen = async () => {
-    lineColor = "#FF00FF";
+  function purplePen() {
+    // lineColor = "#FF00FF";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#FF00FF");
+    console.log(lineColor);
+    setLineColor("#FF00FF");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto")
   };
 
-  const blackPen = async () => {
-    lineColor = "#000000";
+  function blackPen() {
+    // lineColor = "#000000";
     document.querySelector("#board").style.cursor = "pointer";
-    // setLineColor("#000000");
+    console.log(lineColor);
+    setLineColor("#000000");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/iconsmind/outline/16/Pen-5-icon.png'),auto")
   };
 
-  const eraser = async () => {
-    lineColor = "#FFFFFF";
+  function eraser() {
+    // lineColor = "#FFFFFF";
     document.querySelector("#board").style.cursor = "crosshair";
-    // setLineColor("#FFFFFF");
+    console.log(lineColor);
+    setLineColor("#FFFFFF");
     // setCursorStyle("url('https://icons.iconarchive.com/icons/bootstrap/bootstrap/16/Bootstrap-eraser-fill-icon.png'),auto")
   };
 
-  const lineBold = async () => {
-    lineWidth = lineWidth + 1;
-    // setLineWidth(lineWidth+1);
+  function lineBold() {
+    // lineWidth = lineWidth + 1;
+    setLineWidth(lineWidth+1);
   };
 
-  const lineThinner = async () => {
+  function  lineThinner() {
     if (lineWidth <= 1) {
-      lineWidth = lineWidth;
-      // setLineWidth(lineWidth);
+      // lineWidth = lineWidth;
+      setLineWidth(lineWidth);
     } else {
-      lineWidth = lineWidth - 1;
-      // setLineWidth(lineWidth-1);
+      // lineWidth = lineWidth - 1;
+      setLineWidth(lineWidth-1);
     }
   };
 
@@ -263,44 +274,43 @@ const DrawingBoard = ({ role, time, hanleDoSubmit }) => {
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#FF0000" }}
-              // onClick={() => redPen()
-              onClick={() => lineColor="#FF0000" }
+                onClick={() => redPen()}
             ></div>
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#FF7B00" }}
-              // onClick={() => orangePen()}
-              onClick={() => lineColor="#FF7B00" }
+              onClick={() => orangePen()}
+              // onClick={() => lineColor="#FF7B00" }
             ></div>
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#FFFF00" }}
-              // onClick={() => yellowPen()}
-              onClick={() => lineColor="#FFFF00" }
+              onClick={() => yellowPen()}
+              // onClick={() => lineColor="#FFFF00" }
             ></div>
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#00FF00" }}
-              // onClick={() => greenPen()}
-              onClick={() => lineColor="#00FF00" }
+              onClick={() => greenPen()}
+              // onClick={() => lineColor="#00FF00" }
             ></div>
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#0000FF" }}
-              // onClick={() => bluePen()}
-              onClick={() => lineColor="#0000FF" }
+              onClick={() => bluePen()}
+              // onClick={() => lineColor="#0000FF" }
             ></div>
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#FF00FF" }}
-              // onClick={() => purplePen()}
-              onClick={() => lineColor="#FF00FF" }
+              onClick={() => purplePen()}
+              // onClick={() => lineColor="#FF00FF" }
             ></div>
             <div
               className="drawingBoard circle"
               style={{ backgroundColor: "#000000" }}
-              // onClick={() => blackPen()}
-              onClick={() => lineColor="#000000" }
+              onClick={() => blackPen()}
+              // onClick={() => lineColor="#000000" }
             ></div>
           </>
         ) : (
@@ -323,8 +333,8 @@ const DrawingBoard = ({ role, time, hanleDoSubmit }) => {
               clear
             </Button>
             <Button
-              // onClick={() => eraser()}
-              onClick={() => lineColor="#FFFFFF" }
+              onClick={() => eraser()}
+              // onClick={() => lineColor="#FFFFFF" }
               style={{ border: "2px solid #000000", "margin-left": "15px" }}
             >
               eraser
